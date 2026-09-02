fc892a08c841b7ae7650ca3b546e965fa2a294a1
rhulanibee <baloyirhulani72@gmail.com>
2026-09-02 21:15:58 +0200

    Add files via upload

---
+++ authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import AppError from '../utils/AppError.js';
+export default (req, res, next) => {
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(new AppError('You are not logged in! Please log in to get access.', 401));
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return next(new AppError('Token is not valid!', 403));
+  }
+};
---
+++ roleMiddleware.js
@@ -0,0 +1,10 @@
+import AppError from '../utils/AppError.js';
+
+export default (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!allowedRoles.includes(req.user.role)) {
+      return next(new AppError('You do not have permission to perform this action', 403));
+    }
+    next();
+  };
+};
